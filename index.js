c002c7ccd2f6cd54f4e6a6ffadfe92223d59642b
Tori <torihendricks927@yahoo.com>
2022-04-03 21:58:57 -0400

    updated storage and maintaining info on page

--- index.js
+++ index.js
@@ -3,51 +3,14 @@ var saveBtnEl = $('.save');
 var input = $('');
 var hour = $("#Time");
 var container = $('.container');
-var calendar = [
-    {
-    hour: 9,
-    task: "",
-    },
-    {
-     hour: 10,
-    task: "",
-    },
-    {
-    hour: 11,
-    task: "",
-     },
-    {
-     hour: 12,
-    task: "",
-     },
-     {
-    hour: 13,
-    task: "",
-     },
-    {
-    hour: 14,
-    task: "",
-    },
-    {
-     hour: 15,
-    task: "",
-    },
-    {
-     hour: 16,
-    task: "",
-    },
-    {
-     hour: 17,
-    task: "",
-     },
-]
-
+var calendar;
 
 //call all functions
 function init(){
-    timeblock();
-    storeInfo();
     getInfo();
+    timeblock();
+     storeInfo();
+     
 };
 
     // Timer setup
@@ -59,7 +22,7 @@ function init(){
 //Timeblock creates row for each hour
 function timeblock() {
 var now = moment().format("H");
-calendar.forEach(function(ele){
+calendar.forEach(function(ele,index){
     var rowDiv = document.createElement("div");
     if(ele.hour > now) {
         rowDiv.classList.add("future")
@@ -71,12 +34,14 @@ calendar.forEach(function(ele){
 
     var row = `<div class="row">
 <div class="col-2 text-right p-3"> ${ele.hour} </div>
-<input id="${ele.hour}" type="text" class="bg-secondary col border p-3 note colorcode">
-<button class="btn-sm btn-success col-2 p-3 save">
+<input data-index="${index}" id="${ele.hour}" type="text" value="${ele.task}" class="bg-secondary col border p-3 note colorcode">
+<button data-index="${index}" class="btn-sm btn-success col-2 p-3 save">
     <i class="far fa-save"></i>
 </button>
 </div>`;
-
+// var inputID = '#'+ele.hour;
+// console.log(inputID)
+// $("#"+ele.hour).val(calendar[index]["task"]);
 rowDiv.innerHTML = row;
 $(ele.hour).val(ele.task);
 console.log(ele.task)
@@ -88,11 +53,19 @@ container.append(rowDiv);
 container.on('click', "button", function(event) {
     console.log("click")
     // event.PreventDefault();
-    var index = $(".save").index(this);
-    calendar[index]["task"]=$(".note").eq(index).val();
-    console.log($(".note").eq(index))
-    console.log(calendar)
+    var target = $(event.currentTarget)
+    var index = $(target).attr("data-index");
+console.log(index)
+    saveText(index);
+   
 });
+
+function saveText(index) {
+    calendar[index]["task"]=$(".note").eq(index).val();
+    console.log($(".note").eq(index).val())
+    console.log(calendar)
+    storeInfo();
+};
 
 
 
@@ -102,7 +75,48 @@ function storeInfo() {
  };
 
  function getInfo() {
-    calendar = JSON.parse(localStorage.getItem("getInfo"));
+    calendar = JSON.parse(localStorage.getItem("storedInfo"));
+
+    if(!calendar) {
+        calendar = [
+            {
+            hour: 9,
+            task: "",
+            },
+            {
+             hour: 10,
+            task: "",
+            },
+            {
+            hour: 11,
+            task: "",
+             },
+            {
+             hour: 12,
+            task: "",
+             },
+             {
+            hour: 13,
+            task: "",
+             },
+            {
+            hour: 14,
+            task: "",
+            },
+            {
+             hour: 15,
+            task: "",
+            },
+            {
+             hour: 16,
+            task: "",
+            },
+            {
+             hour: 17,
+            task: "",
+             }
+        ];
+    }
      
  }
 
